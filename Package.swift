fa290d1dd8c4ce5d7c0e9ed4e226018f7aa28070
Ryan Won <YOUR_GITHUB_NOREPLY@users.noreply.github.com>
2026-09-08 10:10:46 -0700

    Dynamics SDK 15.0 hot fix

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 /*
  * (c) 2026 BlackBerry Limited. All rights reserved.
  *
- * Version: "15.0.8513.67"
+ * Version: "15.0.8513.75"
  *
  */
 
@@ -30,18 +30,18 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BlackBerryDynamics",
-            url: "https://software.download.blackberry.com/repository/framework/dynamics/ios/15.0.8513.67/BlackBerryDynamics_for_iOS_v15.0.8513.67.zip",
-            checksum: "c63ab5ca50b1e531c5caad505d8bf59fafeccfcade346882be0b272312e1776e"
+            url: "https://software.download.blackberry.com/repository/framework/dynamics/ios/15.0.8513.75/BlackBerryDynamics_for_iOS_v15.0.8513.75.zip",
+            checksum: "507fc22128c5785b8198977c1c290e7a93553a006ce301a79587b8f34e1e2125"
         ),
         .binaryTarget(
             name: "GSEProvider",
-            url: "https://software.download.blackberry.com/repository/framework/dynamics/ios/15.0.8513.67/BlackBerryGSEProvider_for_iOS_v15.0.8513.67.zip",
-            checksum: "981c786649eb402518a5eca2af9dcefee20c3b97e73d0e4672d653a91d773624"
+            url: "https://software.download.blackberry.com/repository/framework/dynamics/ios/15.0.8513.75/BlackBerryGSEProvider_for_iOS_v15.0.8513.75.zip",
+            checksum: "4dd920e0b91beb34c3e4e876a06bf35e28e8f26c9a919e66f990b2c2f2281d91"
         ),
         .binaryTarget(
             name: "BlackBerryDynamicsAutomatedTestSupportLibrary",
-            url: "https://software.download.blackberry.com/repository/framework/dynamics/ios/15.0.8513.67/BlackBerryDynamicsAutomatedTestSupportLibrary_for_iOS_v15.0.8513.67.zip",
-            checksum: "79ee281c53fc6c4c1781199385d78ead9affcf5d321d2bb63500e54bec058e2b"
+            url: "https://software.download.blackberry.com/repository/framework/dynamics/ios/15.0.8513.75/BlackBerryDynamicsAutomatedTestSupportLibrary_for_iOS_v15.0.8513.75.zip",
+            checksum: "3de964933db4f4f2b90466a3e06bc1e62a6973a07ec593e88b3646b580e01c52"
         )
     ]
 )
